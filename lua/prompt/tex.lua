271892fc024fe7e6f073b1ad5373b99b5f9b908f
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-01-27 03:35:11 +0800

    :ambulance: Fix lualatex's kpse

--- lua/prompt/tex.lua
+++ lua/prompt/tex.lua
@@ -1,3 +1,6 @@
+-- luacheck: ignore 111 113
+---@diagnostic disable: undefined-global
+kpse.set_program_name(status.list().luatex_engine)
 local prompt = require 'prompt'
 local utils = require "prompt.utils"
 
