2a0863c25996e718a3a37a06e76e2fde2405cc98
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-01-28 04:24:34 +0800

    :hammer: Add environment variables for luatex

--- shell.nix
+++ shell.nix
@@ -1,10 +1,12 @@
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 with pkgs;
-mkShell {
+mkShell rec {
   name = "prompt-style.lua";
   buildInputs = [
-    (luajit.withPackages (
+    (lua5_3.withPackages (
       p: with p; [
         ansicolors
         argparse
@@ -16,4 +18,10 @@ mkShell {
       ]
     ))
   ];
+  shellHook = ''
+    export LUAINPUTS_luatex='lua;${builtins.elemAt buildInputs 0}/share/lua/5.3'
+    export CLUAINPUTS_luatex='${builtins.elemAt buildInputs 0}/lib/lua/5.3'
+    export LUAINPUTS_luahbtex="$LUAINPUTS_luatex"
+    export CLUAINPUTS_luahbtex="$CLUAINPUTS_luatex"
+  '';
 }
