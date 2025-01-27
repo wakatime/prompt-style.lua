c8cf8f5396cf0e7b721094d75b99b4bd33a7d8a2
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-01-28 04:58:20 +0800

    :hammer: Add environment variables for lualatex

--- lua/prompt/tex.lua
+++ lua/prompt/tex.lua
@@ -1,5 +1,6 @@
 -- luacheck: ignore 111 113
 ---@diagnostic disable: undefined-global
+-- FIXME: for security if you forget add it to `\directlua{}` for lualatex
 kpse.set_program_name(status.list().luatex_engine)
 local prompt = require 'prompt'
 local utils = require "prompt.utils"
--- shell.nix
+++ shell.nix
@@ -3,25 +3,34 @@
 }:
 
 with pkgs;
+let
+  packages = (
+    p: with p; [
+      ansicolors
+      argparse
+      luafilesystem
+      luaprompt
+
+      busted
+      ldoc
+    ]
+  );
+in
 mkShell rec {
   name = "prompt-style.lua";
   buildInputs = [
-    (lua5_3.withPackages (
-      p: with p; [
-        ansicolors
-        argparse
-        luafilesystem
-        luaprompt
-
-        busted
-        ldoc
-      ]
-    ))
+    (lua5_3.withPackages packages)
+    (luajit.withPackages packages)
   ];
   shellHook = ''
-    export LUAINPUTS_luatex='lua;${builtins.elemAt buildInputs 0}/share/lua/5.3'
-    export CLUAINPUTS_luatex='${builtins.elemAt buildInputs 0}/lib/lua/5.3'
+    export LUAINPUTS_luatex="lua;${builtins.elemAt buildInputs 0}/share/lua/5.3"
+    export CLUAINPUTS_luatex="${builtins.elemAt buildInputs 0}/lib/lua/5.3"
+    export LUAINPUTS_luajittex="lua;${builtins.elemAt buildInputs 1}/share/lua/5.1"
+    export CLUAINPUTS_luajittex="${builtins.elemAt buildInputs 1}/lib/lua/5.1"
+
     export LUAINPUTS_luahbtex="$LUAINPUTS_luatex"
     export CLUAINPUTS_luahbtex="$CLUAINPUTS_luatex"
+    export LUAINPUTS_luajithbtex="$LUAINPUTS_luajittex"
+    export CLUAINPUTS_luajithbtex="$CLUAINPUTS_luajittex"
   '';
 }
